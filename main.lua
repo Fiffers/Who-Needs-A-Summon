f66e26556b3d6f3af4a3e50b389e084efec5925b
sjpfeiffer <steven@darebait.com>
2022-01-12 20:39:29 -0800

    Fix bug causing only one member on tooltip

--- main.lua
+++ main.lua
@@ -1,19 +1,16 @@
 local inDebugMode = false
 local switcher = false
+local didPopup = false
+
+neverAgain = false
 
 local GameTooltipLine1 = ""
 local GameTooltipLine2 = ""
 local GameTooltipLine3 = ""
 local savedStoneName = ""
-local locationIDs = {}
-
-local SAVED_PER_CHAR
-
-local _G, print, C_Map, GetRaidRosterInfo, GetNumGroupMembers, IsInGroup, GetRealZoneText, StaticPopup_Show =
-      _G, print, C_Map, GetRaidRosterInfo, GetNumGroupMembers, IsInGroup, GetRealZoneText, StaticPopup_Show
 
 -- RGB and HEX colors for each class through WotLK, because that's all that matters
-local classColors = {
+classColors = {
     deathknight = {r = .77, g = .12, b = .23, hex = "C41F3B"},
     druid = {r = 1, g = .49, b = .04, hex = "FF7D0A"},
     hunter = {r = .67, g = .83, b = .45, hex = "ABD473"},
@@ -31,132 +28,132 @@ local classColors = {
 -- https://wowpedia.fandom.com/wiki/UiMapID/Classic
 
 -- Vanilla Locations - Kalimdor
-locationIDs["RagefireChasm"] = {
+RagefireChasm = {
     1454, "Ragefire Chasm"
 }
 
-locationIDs["WailingCaverns"] = {
+WailingCaverns = {
     1413, 11, 279, 825, "Wailing Caverns"
 }
 
-locationIDs["RazorfenDowns"] = {
+RazorfenDowns = {
     1413, 300, "Razorfen Downs"
 }
 
-locationIDs["RazorfenKraul"] = {
+RazorfenKraul = {
     1413, 301, "Razorfen Kraul"
 }
 
-locationIDs["ZulFarrak"] = {
+ZulFarrak = {
     1446, 219, "Zul'Farrak"
 }
 
-locationIDs["AhnQiraj"] = {
+AhnQiraj = {
     1451, 327, 247, 319, 320, 321, "Ahn'Qiraj", "Ruins of Ahn'Qiraj", "Temple of Ahn'Qiraj"
 }
 
-locationIDs["DireMaul"] = {
+DireMaul = {
     1444, 2324, 235, 236, 237, 238, 239, 240, "Dire Maul"
 }
 
-locationIDs["OnyxiasLair"] = {
+OnyxiasLair = {
     1445, 248, "Onyxia's Lair"
 }
 
-locationIDs["Maraudon"] = {
+Maraudon = {
     1443, 67, 68, 280, 281, "Maraudon"
 }
 
-locationIDs["BlackfathomDeeps"] = {
+BlackfathomDeeps = {
     1440, 221, 222, 223, "Blackfathom Deeps"
 }
 
 -- Vanilla locations - Eastern Kingdoms
-locationIDs["TheDeadmines"] = {
+TheDeadmines = {
     1436, 55, 291, 292, 835, 836, "The Deadmines"
 }
 
-locationIDs["ShadowfangKeep"] = {
+ShadowfangKeep = {
     1421, 311, 312, 313, 314, 315, 316, "Shadowfang Keep"
 }
 
-locationIDs["TheStockade"] = {
+TheStockade = {
     1453, 1013, 225, "The Stockade"
 }
 
-locationIDs["Gnomeregan"] = {
+Gnomeregan = {
     1426, 226, 227, 228, 229, 840, 841, 842, 1371, 1372, 1374, 1380, "Gnomeregan"
 }
 
-locationIDs["ScarletMonastery"] = {
+ScarletMonastery = {
     1420, 19, 302, 303, 304, 305, 435, 436, 804, 805, "Scarlet Monastery"
 }
 
-locationIDs["Uldaman"] = {
+Uldaman = {
     1418, 16, 230, 231, "Uldaman"
 }
 
-locationIDs["TheTempleofAtalHakkar"] = {
+TheTempleofAtalHakkar = {
     1415, 220, "The Temple of Atal'Hakkar"
 }
 
-locationIDs["BlackrockDepths"] = {
+BlackrockDepths = {
     1428, 33, 34, 232, 250, 251, 252, 253, 254, 255, 283, 284, 285, 286, 287, 288, 289, 290, 616, 617, 618, 868, 1538, 1539, 1560, "Blackrock Mountain", "Blackrock Depths", "Blackwing Lair", "Blackrock Spire", "Molten Core"
 }
 
-locationIDs["Scholomance"] = {
+Scholomance = {
     1422, 306, 307, 308, 309, 476, 477, 478, 479, "Scholomance"
 }
 
-locationIDs["Stratholme"] = {
+Stratholme = {
     1423, 317, 318, 1505, 827, "Stratholme"
 }
 
-locationIDs["ZulGurub"] = {
+ZulGurub = {
     1434, 233, 337, "Zul'Gurub"
 }
 
 -- TBC Locations
-locationIDs["Auchindoun"] = {
+Auchindoun = {
     1952, 256, 257, 258, 259, 260, 272, "Auchindoun", "Mana-Tombs", "Sethekk Halls", "Auchenai Crypts", "Shadow Labyrinth"
 }
 
-locationIDs["SerpentshrineCavern"] = {
+SerpentshrineCavern = {
     1946, 262, 263, 264, 265, 332, 1554, "Serpentshrine Cavern", "Slave Pens", "Steam Vault", "Underbog"
 }
 
-locationIDs["BladesEdgeMountains"] = {
+BladesEdgeMountains = {
     1949, 330, "Gruul's Lair"
 }
 
-locationIDs["Netherstorm"] = {
+Netherstorm = {
     1953, 112, 266, 267, 268, 269, 270, 271, 334, 397, 889, 890, 1555, "The Mechanar", "The Botanica", "The Arcatraz", "The Eye"
 }
 
-locationIDs["HellfireCitadel"] = {
+HellfireCitadel = {
     1944, 662, 663, 664, 665, 666, 667, 668, 669, 670, 246, 261, 331, 347, "Hellfire Ramparts", "The Blood Furnace", "The Shattered Halls", "Magtheridon's Lair"
 }
 
-locationIDs["BlackTemple"] = {
+BlackTemple = {
     1948, 339, 490, 540, 541, 574, 575, 576, 582, 759, "The Black Temple", "Black Temple"
 }
 
-locationIDs["Karazhan"] = {
+Karazhan = {
     1430, 350, 351, 352, 353, 354, 355, 356, 357, 358, 359, 360, 361, 362, 363, 364, 365, 366, 794, 795, 796, 797, 809, 810, 811, 812, 813, 814, 815, 816, 817, 818, 819, 820, 821, 822, "Karazhan"
 }
 
-locationIDs["CavernsofTime"] = {
+CavernsofTime = {
     1446, 273, 274, 329, 74, 75, 1552, 1553, "Caverns of Time", "The Black Morass", "Hyjal Summit", "Old Hillsbrad"
 }
 
-locationIDs["ZulAman"] = {
+ZulAman = {
     1942, 333, "Zul'Aman"
 }
 
 
 
-local function getGroupLocations(index)
-    local name, rank, subgroup, level, class, fileName, zone, online, isDead, role, isML = GetRaidRosterInfo(index);
+function getGroupLocations(index)
+    name, rank, subgroup, level, class, fileName, zone, online, isDead, role, isML = GetRaidRosterInfo(index);
     local member = {name = name, location = zone, class = class, hasArrived = false}
     member.uiMapID = C_Map.GetBestMapForUnit(member.name)
     return member
@@ -164,25 +161,23 @@ end
 
 
 
-local function checkWhoHasArrived(stone)
+function checkWhoHasArrived(stone)
     if IsInGroup() then
         for i = 1, GetNumGroupMembers(), 1 do
             local member  = getGroupLocations(i)
             if inDebugMode and member.name ~= "Eztestyay" then
-                if locationIDs[stone] then
-                    for i = 1, #locationIDs[stone], 1 do
-                        if member.uiMapID == locationIDs[stone] then
-                            print("If 1: ", locationIDs[stone], member.uiMapID, member.location)
-                            --member.hasArrived = true
-                        elseif member.location == locationIDs[stone] then
-                            print("If 2: ", locationIDs[stone], member.uiMapID, member.location)
-                        end
+                for i = 1, #RazorfenKraul, 1 do
+                    if member.uiMapID == RazorfenKraul[i] then
+                        print("If 1: ", RazorfenKraul[i], member.uiMapID, member.location)
+                    elseif member.location == RazorfenKraul[i] then
+                        print("If 2: ", RazorfenKraul[i], member.uiMapID, member.location)
                     end
                 end
             end
-            if locationIDs[stone] then
-                for i = 1, #locationIDs[stone], 1 do
-                    if member.uiMapID == locationIDs[stone][i] or member.location == locationIDs[stone][i] then
+            if _G[stone] then
+                for i = 1, #_G[stone], 1 do
+                    if member.uiMapID == _G[stone][i] or member.location == _G[stone][i] then
+                        print(_G[stone][i], member.uiMapID, member.location)
                         member.hasArrived = true
                     end
                 end
@@ -193,14 +188,13 @@ local function checkWhoHasArrived(stone)
                     end
                     GameTooltip:AddLine(member.name, classColors[member.class:lower()].r, classColors[member.class:lower()].g, classColors[member.class:lower()].b, false)
                     GameTooltip:Show()
-                    return member.name
                 end
             end
         end
     end
 end
 
--- CreateEvent when GameToolTip Shows
+-- CreateEvent when GameToolTip Update
 local function ToolTipOnShow()
     switcher = false
     if not (GameTooltipLine1 == GameTooltipTextLeft1:GetText() and GameTooltipLine2 == GameTooltipTextLeft2:GetText() and GameTooltipLine3 == GameTooltipTextLeft3:GetText()) then
@@ -209,8 +203,9 @@ local function ToolTipOnShow()
         GameTooltipLine3 = GameTooltipTextLeft3:GetText()
 
         if GameTooltipLine1 == "Meeting Stone" then
-            if not SAVED_PER_CHAR.neverAgain then
-                StaticPopup_Show("WhoNeedsASummon_WELCOME")
+            if not didPopup and not neverAgain then
+                StaticPopup_Show ("WELCOME")
+                didPopup = true
             end
             savedStoneName = GameTooltipLine2
             GameTooltipLine2 = GameTooltipLine2:gsub("%s+", "")
@@ -223,25 +218,6 @@ end
 GameTooltip:HookScript("OnShow", ToolTipOnShow)
 
 -- CreateEvent when GameToolTip Update
-local function ToolTipOnUpdate()
-    switcher = false
-    if not (GameTooltipLine1 == GameTooltipTextLeft1:GetText() and GameTooltipLine2 == GameTooltipTextLeft2:GetText() and GameTooltipLine3 == GameTooltipTextLeft3:GetText()) then
-        GameTooltipLine1 = GameTooltipTextLeft1:GetText()
-        GameTooltipLine2 = GameTooltipTextLeft2:GetText()
-        GameTooltipLine3 = GameTooltipTextLeft3:GetText()
-
-        if GameTooltipLine1 == "Meeting Stone"  and GameTooltipLine3 == nil then
-            savedStoneName = GameTooltipLine2
-            GameTooltipLine2 = GameTooltipLine2:gsub("%s+", "")
-            GameTooltipLine2 = GameTooltipLine2:gsub("'", "")
-            checkWhoHasArrived(GameTooltipLine2)
-        end
-    end
-end
-
-GameTooltip:HookScript("OnUpdate", ToolTipOnUpdate)
-
--- CreateEvent when GameToolTip Hides
 local function ToolTipOnHide()
     if not (GameTooltipLine1 == GameTooltipTextLeft1:GetText() and GameTooltipLine2 == GameTooltipTextLeft2:GetText() and GameTooltipLine3 == GameTooltipTextLeft3:GetText()) then
         GameTooltipLine1 = GameTooltipTextLeft1:GetText()
@@ -256,27 +232,11 @@ if inDebugMode then
     C_Timer.NewTicker(10, checkWhoHasArrived)
 end
 
-
-local frame = CreateFrame("Frame")
-frame:SetScript("OnEvent", function(_, event, ...)
-    local arg1 = ...
-    if event == "ADDON_LOADED" and arg1 == "WhoNeedsASummon" then
-        frame:UnregisterEvent("ADDON_LOADED")
-        
-        if _G.WhoNeedsASummon_PerCharacter == nil then
-            _G.WhoNeedsASummon_PerCharacter = { neverAgain = _G.neverAgain } -- copy the old savedvariable
-        end
-        SAVED_PER_CHAR = _G.WhoNeedsASummon_PerCharacter
-    end
-end)
-frame:RegisterEvent("ADDON_LOADED")
-
-
-_G.SLASH_GETLOC1 = "/getloc"
+SLASH_GETLOC1 = "/getloc"
 
 SlashCmdList["GETLOC"] = function()
     local uiMapId = C_Map.GetBestMapForUnit("Player")
-    local printStr = "Please give the following |cff00ff98information|r to the developer by commenting on the addon page on |cffff7017CurseForge|r or whispering |cff7289daFyfor#0403|r on Discord: |cff00ff98\nZone Name:|r " .. GetRealZoneText()
+    local printStr = "Please give the following |cff00ff98information|r to the developer by commenting on the addon page on |cffff7017CurseForge|r or whispering |cff7289daMooMooMoo#0403|r on Discord: |cff00ff98\nZone Name:|r " .. GetRealZoneText()
     if uiMapId then
         printStr = printStr ..   "\n|cff00ff98Zone ID:|r " .. uiMapId
     end
@@ -289,12 +249,12 @@ SlashCmdList["GETLOC"] = function()
     savedStoneName = ""
 end
 
-StaticPopupDialogs["WhoNeedsASummon_WELCOME"] = {
+StaticPopupDialogs["WELCOME"] = {
   text = "Thank you for downloading |cff00ff98\"Who needs a summon?\"|r This addon is still very much in development. If you run into any issues with names incorrectly displaying on meeting stones, please type |cff00ff98/getloc|r and follow the instructions given. Happy gaming!",
   button1 = "Will do",
   button2 = "Never show this again",
   OnCancel = function()
-     SAVED_PER_CHAR.neverAgain = true
+     neverAgain = true
   end,
   timeout = 0,
   whileDead = true,
